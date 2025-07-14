0fc4789eb31b309b1f1205a1a6ca43c56b06b094
KIMjongYoun1 <oomg4297@gmail.com>
2025-07-14 12:43:04 +0900

    포켓몬 그리드컴포넌트

---
+++ frontend/src/components/PokemonGrid.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import type { PokemonDTO } from "../types/Pokemon";
+
+interface PokemonGridProps {
+    searchResults: PokemonDTO[];
+    onPokemonSelect: (pokemon: PokemonDTO) => void;
+}
+
+function PokemonGrid({ searchResults, onPokemonSelect }: PokemonGridProps) {
+    if (searchResults.length === 0) {
+        return null;
+    }
+
+    return (
+        <div className="search-results">
+            <h2>검색 결과 ({searchResults.length}개)</h2>
+            <div className="pokemon-grid">
+                {searchResults.map((pokemon) => (
+                    <div key={pokemon.id} className="pokemon-grid-item" onClick={() => onPokemonSelect(pokemon)}>
+                        <img src={pokemon.spriteUrl} alt={pokemon.name} />
+                        <h3>{pokemon.name.toUpperCase()}</h3>
+                        <p>#{pokemon.pokemonId}</p>
+                        <div className="types">
+                            {pokemon.types.map((type, index) => (
+                                <span key={index} className="type-badge small">
+                                    {type}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default PokemonGrid;
